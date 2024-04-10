1b3829cf5cac66b139c47178c3602a27d7b3e01e
Jonathan Leek <mrjonleek@gmail.com>
2024-04-09 20:07:32 -0700

    Optimization and consolidation

--- src/app/new-token/new-token.component.ts
+++ src/app/new-token/new-token.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { Router } from '@angular/router';
-import { ModalController, IonHeader, IonContent, IonToolbar, IonTitle, IonInput, IonModal, IonButton, IonButtons, IonItem, IonList } from "@ionic/angular/standalone";
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { IonHeader, IonContent, IonToolbar, IonTitle, IonInput, IonModal, IonButton, IonButtons, IonItem, IonList } from "@ionic/angular/standalone";
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 
 import { TokenService } from 'src/app/token.service';
 
@@ -17,57 +17,43 @@ import { TokenService } from 'src/app/token.service';
 })
 export class NewTokenComponent implements OnInit {
 
+  tokenService = inject(TokenService);
+
   newAccountForm!: FormGroup;
 
   /**
    * 
    * @param formBuilder 
    * @param router 
-   * @param tokenService 
    */
   constructor(
     private formBuilder: FormBuilder,
-    private router: Router,
-    private tokenService: TokenService
+    private router: Router
   ) { }
 
-  /**
-   * 
-   */
   ngOnInit(): void {
     this.newAccountForm = this.formBuilder.group({
       issuer: [null, Validators.required],
       label: [''],
-      secret: ['I65VU7K5ZQL7WB4E', [Validators.required, Validators.pattern(/^[A-Z2-7]+=*$/)]]
+      secret: ['I65VU7K5ZQL7WB4E', [Validators.required, Validators.pattern(/^[A-Z2-7]+=*$/), this.base32Challenge()]]
     })
   }
-
-  // base32Challenge(secret: string): ValidatorFn {
-  //   return (control: AbstractControl): ValidationErrors | null => {
-  //     const forbidden = control.value.length % 8 === 0;
-  //     console.log(forbidden);
-  //     return forbidden ? { forbiddenName: { value: control.value } } : null;
-  //   };
-  // }
 
   /**
    * The base32Challenge can be used to validate
    * the input passed as the token secret.
-   * 
-   * @param test 
    */
-  base32Challenge(test: string) {
-    try {
-      test.length % 8 === 0 && (/^[A-Z2-7]+=*$/).test(test);
-    } catch (error) {
-      console.error(error);
-      throw Error("Invalid token secret.");
-    }
+  base32Challenge(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const forbidden = control.value.length % 8 === 0;
+      console.log(forbidden);
+      return !forbidden ? { invalideBase32: { value: control.value } } : null;
+    };
   }
 
   async submitNewAccountForm() {
     if (this.newAccountForm.valid) {
-      this.tokenService.saveToken(this.newAccountForm.value);
+      this.tokenService.create(this.newAccountForm.value);
       this.router.navigate(['/']);
     }
   }
